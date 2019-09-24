32429dac0a473ef0137d1f90edb99d1f94d2053e
Fukuda Naoto <fukuda.naoto@nifty.co.jp>
2019-09-24 13:33:30 +0900

    Add organization val

--- index.js
+++ index.js
@@ -10,33 +10,37 @@ const SOURCE_REPO_URL = process.env["SOURCE_REPO_URL"];
 const SOURCE_API_URL = process.env["SOURCE_API_URL"];
 const SOURCE_REPO_OWNER = process.env["SOURCE_REPO_OWNER"];
 const SOURCE_REPO_NAME = process.env["SOURCE_REPO_NAME"];
+const SOURCE_REPO_ORGANIZATION = process.env["SOURCE_REPO_ORGANIZATION"];
 
 const TARGET_GH_TOKEN = process.env["TARGET_GH_TOKEN"];
 const TARGET_REPO_URL = process.env["TARGET_REPO_URL"];
 const TARGET_API_URL = process.env["TARGET_API_URL"];
 const TARGET_REPO_OWNER = process.env["TARGET_REPO_OWNER"];
 const TARGET_REPO_NAME = process.env["TARGET_REPO_NAME"];
+const TARGET_REPO_ORGANIZATION = process.env["TARGET_REPO_ORGANIZATION"];
 
 (async () => {
   const sourceRepo = new Repository(
     SOURCE_REPO_NAME,
     SOURCE_REPO_OWNER,
     SOURCE_API_URL,
-    SOURCE_GH_TOKEN
+    SOURCE_GH_TOKEN,
+    SOURCE_REPO_ORGANIZATION
   );
   const targetRepo = new Repository(
     TARGET_REPO_NAME,
     TARGET_REPO_OWNER,
     TARGET_API_URL,
-    TARGET_GH_TOKEN
+    TARGET_GH_TOKEN,
+    TARGET_REPO_ORGANIZATION
   );
   const desc = await sourceRepo.getRepositoryDescription();
-  await targetRepo.createRemoteRepository(desc);
+  // await targetRepo.createRemoteRepository(desc);
   transferRepository(SOURCE_REPO_URL, TARGET_REPO_URL);
   const issues = await sourceRepo.getIssues();
   console.log(issues);
-  await targetRepo.setIssues(issues);
+  // await targetRepo.setIssues(issues);
   const hooks = await sourceRepo.getHooks();
   console.log(hooks);
-  await targetRepo.setHooks(hooks);
+  // await targetRepo.setHooks(hooks);
 })();
